da61de86b980eb681e72fd498b42599287bc4180
josejimenezr <53203772+josejimenezr@users.noreply.github.com>
2019-10-10 22:12:30 +1100

    Add files via upload

--- db_connection.php
+++ db_connection.php
@@ -9,10 +9,10 @@ File specification: Connect the adoption database
 -->
 <?php
     // Predifine attributes to login into the database
-	$db_address = process.env.DB_ADDRESS;
-	$db_user = process.envDB_USERNAME;
-	$db_pass = process.env.DB_PASSWORD;
-	$db_name = process.env.DB_NAME;     // database name
+	$db_address = getenv('DB_ADDRESS');
+	$db_user = getenv('DB_USERNAME');
+	$db_pass = getenv('DB_PASSWORD');
+	$db_name = getenv('DB_NAME');     // database name
 
     // create new variable $db that is the database connection
 	$db = new mysqli($db_address, $db_user, $db_pass, $db_name);
